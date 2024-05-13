8604212420490d4e856d55c439d2eb86316f2687
jgunstone <j.gunstone@maxfordham.com>
2024-05-13 12:03:40 +0100

    update reader

--- src/xlsxdatagrid/read.py
+++ src/xlsxdatagrid/read.py
@@ -1,17 +1,52 @@
-from python_calamine import CalamineWorkbook
+from python_calamine import CalamineWorkbook, CalamineSheet
 import typing as ty
 from pydantic import BaseModel
+from xlsxdatagrid.xlsxdatagrid import DataGridMetaData
+from stringcase import snakecase
+from pathlib import Path
+
+from tempfile import TemporaryDirectory
+from datamodel_code_generator import InputFileType, generate
+from datamodel_code_generator import DataModelType
+import importlib.util
+import sys
+import json
 
 
-def read_metadata(s):
+def pydantic_model_from_json_schema(json_schema: str) -> ty.Type[BaseModel]:
+    load = json_schema["title"] if "title" in json_schema else "Model"
+
+    with TemporaryDirectory() as temporary_directory_name:
+        temporary_directory = Path(temporary_directory_name)
+        file_path = "model.py"
+        module_name = file_path.split(".")[0]
+        output = Path(temporary_directory / file_path)
+        generate(
+            json.dumps(json_schema),
+            input_file_type=InputFileType.JsonSchema,
+            input_filename="example.json",
+            output=output,
+            output_model_type=DataModelType.PydanticV2BaseModel,
+        )
+        spec = importlib.util.spec_from_file_location(module_name, output)
+        module = importlib.util.module_from_spec(spec)
+        sys.modules[module_name] = module
+        spec.loader.exec_module(module)
+    return getattr(module, load)
+
+
+def read_metadata(s: str) -> DataGridMetaData:
     s = s.replace("#", "")
     li = [l.split("=") for l in s.split(" - ")]
-    return {l[0]: l[1] for l in li}
+    di = {snakecase(l[0]): l[1] for l in li}
+    return DataGridMetaData(**di)
 
 
-def process_data(data, metadata):
-    hd = int(metadata.get("HeaderDepth"))
-    is_t = eval(metadata.get("IsTransposed"))
+def process_data(
+    data: list[dict], metadata: DataGridMetaData
+) -> tuple[list[dict], DataGridMetaData]:
+    hd = metadata.header_depth
+    is_t = metadata.is_transposed
     if is_t:
         data = list(map(list, zip(*data)))
 
@@ -20,7 +55,8 @@ def process_data(data, metadata):
     data = [d[1:] for d in data]
     headers = {h: data[n] for n, h in enumerate(header_names)}
     header = headers[header_names[-1]]
-    metadata["Headers"] = headers
+    metadata.datagrid_index_name = list(headers.keys())
+    metadata.header = list(headers.values())
 
     data = data[len(header_names) :]
     data = [dict(zip(header, d)) for d in data]
@@ -28,7 +64,7 @@ def process_data(data, metadata):
     return data, metadata
 
 
-def read_data(data):
+def read_data(data) -> tuple[list[dict], DataGridMetaData]:
     if data[0][0][0] != "#":
         raise ValueError(
             "the first row must be a metadata string beginning with the char '#'"
@@ -38,21 +74,34 @@ def read_data(data):
     return process_data(data, metadata)
 
 
-def load_data(data, metadata, loader: ty.Callable[[str], ty.Type[BaseModel]]):
-    name = metadata.get("Schema")
-    model = loader(name)
-    if model is not None:
-        return model.model_validate(data)
+def get_jsonschema(metadata: DataGridMetaData) -> dict:
+    pass
+
+
+def read_worksheet(
+    worksheet: CalamineSheet,
+    get_jsonschema: ty.Optional[ty.Callable[[DataGridMetaData], dict]] = None,
+) -> list[dict]:
+    data = worksheet.to_python(skip_empty_area=True)
+    data, metadata = read_data(data)
+    if get_jsonschema is not None:
+        json_schema = get_jsonschema(metadata)
+        if json_schema is not None:
+            pydantic_model = pydantic_model_from_json_schema(json_schema)
+            return pydantic_model.model_validate(data).model_dump(mode="json")
+        else:
+            return data
     else:
         return data
 
 
-def read_excel(path, model_loader=None):
+def read_excel(
+    path,
+    get_jsonschema: ty.Optional[
+        ty.Callable[[DataGridMetaData], ty.Type[BaseModel]]
+    ] = None,
+):
     workbook = CalamineWorkbook.from_path(path)
     sheet = workbook.sheet_names[0]
-    data = workbook.get_sheet_by_name(sheet).to_python(skip_empty_area=True)
-    data, metadata = read_data(data)
-    if model_loader is not None:
-        return load_data(data, metadata, model_loader)
-    else:
-        return data
+    worksheet = workbook.get_sheet_by_name(sheet)
+    return read_worksheet(worksheet, get_jsonschema)
--- tests/test_read.py
+++ tests/test_read.py
@@ -1,4 +1,5 @@
-from xlsxdatagrid.read import read_excel
+from xlsxdatagrid.read import read_excel, pydantic_model_from_json_schema
+from xlsxdatagrid.xlsxdatagrid import DataGridMetaData
 from .constants import PATH_XL, PATH_XL_TRANSPOSED
 from .test_xlsxdatagrid import TestArray, TestArrayTransposed
 import typing as ty
@@ -13,54 +14,42 @@ from datamodel_code_generator import DataModelType
 import importlib.util
 import sys
 
-paths = list(pathlib.Path("tests").glob("*schema.*"))
-schemas = {}
-for p in paths:
-    schema = json.loads(p.read_text())
-    schemas[schema["title"]] = schema
+# PATH_SCHEMA = pathlib.Path()
+schemas = [TestArray.model_json_schema(), TestArrayTransposed.model_json_schema()]
+schemas = {s["title"]: s for s in schemas}
+
+# paths = list(pathlib.Path("tests").glob("*schema.*"))
+# for p in paths:
+#     schema = json.loads(p.read_text())
+#     schemas[schema["title"]] = schema
 
 
-def model_loader(name):
-    if name in globals():
-        return globals()[name]
-    else:
-        return None
-
-
-def pydantic_model_from_json_schema(json_schema):
-    with TemporaryDirectory() as temporary_directory_name:
-        temporary_directory = Path(temporary_directory_name)
-        file_path = "model.py"
-        module_name = file_path.split(".")[0]
-        output = Path(temporary_directory / file_path)
-        generate(
-            json_schema,
-            input_file_type=InputFileType.JsonSchema,
-            input_filename="example.json",
-            output=output,
-            output_model_type=DataModelType.PydanticV2BaseModel,
-        )
-        spec = importlib.util.spec_from_file_location(module_name, output)
-        module = importlib.util.module_from_spec(spec)
-        sys.modules[module_name] = module
-        spec.loader.exec_module(module)
-    return module.Model
-
-
-@pytest.mark.parametrize("path", [PATH_XL, PATH_XL_TRANSPOSED])
-def test_read_excel(path):
-    obj = read_excel(path, model_loader=model_loader)
-    assert obj.model_json_schema()["datagrid_index_name"] == (
-        "section",
-        "title",
-        "name",
-    )
-    assert isinstance(obj.model_dump(), list)
-    print("done")
+# def pydantic_model_from_json_schema(json_schema: str) -> ty.Type[BaseModel]:
+#     with TemporaryDirectory() as temporary_directory_name:
+#         temporary_directory = Path(temporary_directory_name)
+#         file_path = "model.py"
+#         module_name = file_path.split(".")[0]
+#         output = Path(temporary_directory / file_path)
+#         generate(
+#             json_schema,
+#             input_file_type=InputFileType.JsonSchema,
+#             input_filename="example.json",
+#             output=output,
+#             output_model_type=DataModelType.PydanticV2BaseModel,
+#         )
+#         spec = importlib.util.spec_from_file_location(module_name, output)
+#         module = importlib.util.module_from_spec(spec)
+#         sys.modules[module_name] = module
+#         spec.loader.exec_module(module)
+#     return module.Model
 
 
 def get_schema(name, schemas=schemas):
     return schemas.get(name)
+
+
+def get_jsonschema(metadata: DataGridMetaData) -> dict:
+    return schemas.get(metadata.template_name)
 
 
 TEST_JSON_SCHEMA: str = """{
@@ -76,11 +65,21 @@ TEST_JSON_SCHEMA: str = """{
 
 
 def test_load_model_from_json_schema():
-    pydantic_model = pydantic_model_from_json_schema(TEST_JSON_SCHEMA)
+    pydantic_model = pydantic_model_from_json_schema(json.loads(TEST_JSON_SCHEMA))
     assert issubclass(pydantic_model, BaseModel)
     assert isinstance(pydantic_model(), BaseModel)
 
 
-def load_model_from_xl_metadata():
+def test_get_jsonschema():
 
-    pass
+    metadata = DataGridMetaData(template_name="TestArray")
+    jsonschema = get_jsonschema(metadata)
+    assert jsonschema["title"] == "TestArray"
+
+
+@pytest.mark.parametrize("path", [PATH_XL, PATH_XL_TRANSPOSED])
+def test_read_excel(path):
+    obj = read_excel(path, get_jsonschema=get_jsonschema)
+    assert isinstance(obj, list)
+    assert len(obj) == 3
+    print("done")
